b460635fec23806f8fd33bf0eae8dbe0c382b451
mmondani <mmondani89@gmail.com>
2021-08-11 19:59:33 -0300

    Se cambia la conexión a la base de datos por un pool de conexiones

--- src/mysql-connector.js
+++ src/mysql-connector.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+    connectionLimit: 10,
     host     : 'mysql-server',
     port     : '3306',
     user     : 'root',
@@ -8,13 +9,26 @@ var connection = mysql.createConnection({
     database : 'DAM'
 });
 
-
-connection.connect(function(err) {
+// Conexión de prueba
+pool.getConnection((err, connection) => {
     if (err) {
-        console.error('Error while connect to DB: ' + err.stack);
+        switch (err.code) {
+            case 'PROTOCOL_CONNECTION_LOST':
+                console.error('La conexion a la DB se cerró.');
+                break;
+            case 'ER_CON_COUNT_ERROR':
+                console.error('La base de datos tiene muchas conexiones');
+                break;
+            case 'ECONNREFUSED':
+                console.error('La conexion fue rechazada');
+        }
+
+        if (connection) {
+            connection.release();
+        }
+
         return;
     }
-    console.log('Connected to DB under thread ID: ' + connection.threadId);
 });
 
-module.exports = connection;
+module.exports = pool;
